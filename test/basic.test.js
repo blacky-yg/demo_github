da8cf2abb833a23207e4d5ed3dced49abcff9c0d
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-08-22 11:25:37 +0100

    update workflow

---
+++ test/basic.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { describe } = require('mocha');
+
+describe('Basic Mocha String Test', () => {
+    it('should return number of charachters in a string', () => {
+        assert.equal('Hello'.length, 5);
+    });
+    it('should return first charachter of the string', () => {
+        assert.equal('Hello'.charAt(0), 'H');
+    });
+});
