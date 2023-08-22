ea9af311008447913fb7b01195a18a1fbe8159c3
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-08-22 16:48:55 +0100

    change test

--- test/basic.test.js
+++ test/basic.test.js
@@ -3,7 +3,7 @@ const { describe } = require('mocha');
 
 describe('Basic Mocha String Test', () => {
     it('should return number of charachters in a string', () => {
-        assert.equal('Hello'.length, 5);
+        assert.equal('Hello'.length, 4);
     });
     it('should return first charachter of the string', () => {
         assert.equal('Hello'.charAt(0), 'H');
